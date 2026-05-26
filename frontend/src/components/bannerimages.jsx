29b300dd4b4701a721acb07600807129010cc45e
Criz <crizvc3@gmail.com>
2026-05-26 12:29:42 -0500

    banner efecto

--- frontend/src/components/bannerimages.jsx
+++ frontend/src/components/bannerimages.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import imagenBanner from "../assets/img1.jpeg"
+import React, { useState } from 'react';
+import imagenBanner from "../assets/img1.jpeg";
+import imagenBanner2 from "../assets/img2.jpg";
 
 function BannerImages(props) {
+    const [hovered, setHovered] = useState(false);
+
     return (
         <div className="mt-10 bannerImg">
 
@@ -9,11 +12,32 @@ function BannerImages(props) {
                 <h1>Animes</h1>
                 <h3>Recientemente Agregados</h3>
                 <hr className='hrStyle' />
-                <button className='bannerBtn' style={{ marginTop: "20px" }} onClick={props.onExplore}>
+                <button
+                    className='bannerBtn'
+                    style={{ marginTop: "20px" }}
+                    onClick={props.onExplore}
+                    onMouseEnter={() => setHovered(true)}
+                    onMouseLeave={() => setHovered(false)}
+                >
                     Explora Catálogo
                 </button>
             </div>
-            <img src={imagenBanner} alt="" />
+
+            {/* Crossfade container */}
+            <div className="bannerImgContainer">
+                <img
+                    src={imagenBanner}
+                    alt="Banner 1"
+                    className="bannerImgLayer"
+                    style={{ opacity: hovered ? 0 : 1 }}
+                />
+                <img
+                    src={imagenBanner2}
+                    alt="Banner 2"
+                    className="bannerImgLayer"
+                    style={{ opacity: hovered ? 1 : 0 }}
+                />
+            </div>
         </div>
     );
 }
